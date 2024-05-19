8773dcbdd3a08b9e6d3858f6cdc55db88efc7d2b
Huy Chau <chaudanghuy@gmail.com>
2024-05-19 16:58:29 +0700

    Microservice

--- backend/apps/restaurant/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Food
-
-class FoodForm(forms.ModelForm):
-  class Meta:
-        model = Food
-        fields = ['category','name', 'description', 'price', 'image', 'availability', 'translation_id']
